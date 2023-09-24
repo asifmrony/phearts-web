529cbd9077cd9002e78663793403f80e16e89297
Asif Mahmud Rony <asifmrony@gmail.com>
2023-09-25 01:37:28 +0600

    font fix all pages

--- pages/donation.js
+++ pages/donation.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function donation() {
   return (
     <div className='min-h-[100vh] bg-white flex justify-center items-center'>
-        <h1 className='font-smheading'>Coming Soon...</h1>
+        <h1 className={`font-smheading ${poppins.variable} font-poppins`}>Coming Soon...</h1>
     </div>
   )
 }
--- pages/jobs/index.js
+++ pages/jobs/index.js
@@ -1,4 +1,4 @@
-import { poppinsBold } from '@/utils/fonts'
+import { poppins, poppinsBold } from '@/utils/fonts'
 import Link from 'next/link'
 
 export default function jobs() {
@@ -12,19 +12,19 @@ export default function jobs() {
                             <div className='md:space-y-8 md:block flex flex-wrap gap-x-6 justify-between items-center'>
                                 <div className='basis-[45%]'>
                                     <h5 className={`font-subheading ${poppinsBold.variable} font-poppins mb-4`}>Communications Officers</h5>
-                                    <p className='text-sm text-[#d9d9d9]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultrices ipsum non mattis pharetra. Integer laoreet non felis amet.</p>
+                                    <p className={`text-sm text-[#d9d9d9] ${poppins.variable} font-poppins`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultrices ipsum non mattis pharetra. Integer laoreet non felis amet.</p>
                                     <Link href="/jobs/entry" className='inline-block mt-5 uppercase text-xs text-[#e5e5e5] font-bold tracking-wide'>Learn More</Link>
                                 </div>
                                 <hr />
                                 <div className='basis-[45%]'>
                                     <h5 className={`font-subheading ${poppinsBold.variable} font-poppins mb-4`}>HeadQuarters Intern</h5>
-                                    <p className='text-sm text-[#d9d9d9]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultrices ipsum non mattis pharetra. Integer laoreet non felis amet.</p>
+                                    <p className={`text-sm text-[#d9d9d9] ${poppins.variable} font-poppins`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultrices ipsum non mattis pharetra. Integer laoreet non felis amet.</p>
                                     <Link href="/jobs/entry" className='inline-block mt-5 uppercase text-xs text-[#e5e5e5] font-bold tracking-wide'>Learn More</Link>
                                 </div>
                                 <hr />
                                 <div className='basis-[45%] mt-8 md:mt-0'>
                                     <h5 className={`font-subheading ${poppinsBold.variable} font-poppins mb-4`}>Child Protection Expert</h5>
-                                    <p className='text-sm text-[#d9d9d9]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultrices ipsum non mattis pharetra. Integer laoreet non felis amet.</p>
+                                    <p className={`text-sm text-[#d9d9d9] ${poppins.variable} font-poppins`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultrices ipsum non mattis pharetra. Integer laoreet non felis amet.</p>
                                     <Link href="/jobs/entry" className='inline-block mt-5 uppercase text-xs text-[#e5e5e5] font-bold tracking-wide'>Learn More</Link>
                                 </div>
                             </div>
