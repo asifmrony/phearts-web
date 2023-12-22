69d1f40c62f9f53b49641d34c99513d0905eea38
Asif Mahmud Rony <asifmrony@gmail.com>
2023-12-22 10:59:06 +0600

    jobs module data integration v1.00

--- pages/jobs/[entry].js
+++ pages/jobs/[entry].js
@@ -1,13 +1,55 @@
 import PageHeader from '@/components/reusable/PageHeader'
 import React from 'react'
+import ReactMarkdown from 'react-markdown'
 
-export default function single() {
+export const getStaticPaths = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/jobs`);
+  const alljobs = await res.json();
+
+  const paths = alljobs.data.map((body) => ({
+      params: { entry: body.id.toString() }
+  }))
+
+  // paths will be like
+  // [
+  //   {
+  //     params: {slug: '1'}
+  //   },
+  //   {
+  //     params: {slug: '2'}
+  //   }
+  // ]
+  return {
+      paths,
+      fallback: true
+  }
+}
+
+export const getStaticProps = async ({ params }) => {
+  const path = params.entry;
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/jobs/${path}`, {
+      method: 'GET',
+      headers: {
+          'content-type': 'application/json',
+      }
+  });
+  const singleJob = await res.json();
+  return {
+      props: { singleJob }
+  }
+}
+
+
+export default function single({ singleJob }) {
+  console.log("Single Job", singleJob);
+  const {job_title, job_description, job_apply_instruction } = singleJob?.data.attributes;
+
   return (
     <main className='page-jobs-single'>
-      <PageHeader title={'Communications Officer'} subtitle={'Jobs / opening jobs'} background={'transparent'} />
+      <PageHeader title={`${job_title}`} subtitle={'Jobs / opening jobs'} background={'transparent'} />
       <div className='pb-48 bg-white'>
-        <div className='hero-width w-full mx-auto text-center'>
-          <div>
+        <div className='hero-width w-full mx-auto jobs__body'>
+          {/* <div>
             <h2 className='text-2xl font-bold mb-4 text-left'>Principal Accountabilities</h2>
             <ul className='list-disc text-left'>
               <li>Develop and implement effective communications activities for external customers and stakeholders.</li>
@@ -30,13 +72,17 @@ export default function single() {
               <li>Ability to work as part of a team</li>
               <li>Knowledge and a good understanding of Microsoft Office</li>
             </ul>
-          </div>
-          <div className='mt-12'>
-            <p>If reading this excites you, Send your CV to info@phearts.com</p>
-            <p>Please write subject line as “Communications Officer” and send your updated CV with a portfolio and a cover letter.</p>
+          </div> */}
+          {<ReactMarkdown>{job_description}</ReactMarkdown>}
+          <div className='mt-12 text-center'>
+            {/* <p>If reading this excites you, Send your CV to info@phearts.com</p>
+            <p>Please write subject line as “Communications Officer” and send your updated CV with a portfolio and a cover letter.</p> */}
+            {<ReactMarkdown>{job_apply_instruction}</ReactMarkdown>}
           </div>
         </div>
       </div>
     </main>
   )
 }
+
+
--- pages/jobs/index.js
+++ pages/jobs/index.js
@@ -1,7 +1,9 @@
 import { poppins, poppinsBold } from '@/utils/fonts'
 import Link from 'next/link'
+import React from 'react';
 
-export default function jobs() {
+export default function jobs(alljobs) {
+    console.log("ALA jobs", alljobs);
     return (
         <main>
             <div className="page-services__vacancies">
@@ -10,7 +12,21 @@ export default function jobs() {
                         <div className='md:max-w-[500px] md:ml-auto py-14 md:py-28 px-10 md:px-20 text-center md:text-right space-y-8'>
                             <h2 className='font-mdheading mb-10 md:mb-14 uppercase'>Current Vacancies</h2>
                             <div className='md:space-y-8 md:block flex flex-wrap gap-x-6 justify-between items-center'>
-                                <div className='basis-[45%]'>
+                                {alljobs?.data.map((item) => {
+                                    const { job_title, job_summary } = item.attributes;
+
+                                    return (
+                                        <React.Fragment key={item.id}>
+                                            <div className='basis-[45%]'>
+                                                <h5 className={`font-subheading ${poppinsBold.variable} font-poppins mb-4`}>{job_title}</h5>
+                                                <p className={`text-sm text-[#d9d9d9] ${poppins.variable} font-poppins`}>{job_summary}</p>
+                                                <Link href={`/jobs/${item.id}`} className='inline-block mt-5 uppercase text-xs text-[#e5e5e5] font-bold tracking-wide'>Learn More</Link>
+                                            </div>
+                                            <hr />
+                                        </React.Fragment>
+                                    )
+                                })}
+                                {/* <div className='basis-[45%]'>
                                     <h5 className={`font-subheading ${poppinsBold.variable} font-poppins mb-4`}>Communications Officers</h5>
                                     <p className={`text-sm text-[#d9d9d9] ${poppins.variable} font-poppins`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultrices ipsum non mattis pharetra. Integer laoreet non felis amet.</p>
                                     <Link href="/jobs/entry" className='inline-block mt-5 uppercase text-xs text-[#e5e5e5] font-bold tracking-wide'>Learn More</Link>
@@ -26,7 +42,7 @@ export default function jobs() {
                                     <h5 className={`font-subheading ${poppinsBold.variable} font-poppins mb-4`}>Child Protection Expert</h5>
                                     <p className={`text-sm text-[#d9d9d9] ${poppins.variable} font-poppins`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultrices ipsum non mattis pharetra. Integer laoreet non felis amet.</p>
                                     <Link href="/jobs/entry" className='inline-block mt-5 uppercase text-xs text-[#e5e5e5] font-bold tracking-wide'>Learn More</Link>
-                                </div>
+                                </div> */}
                             </div>
                         </div>
                     </div>
@@ -35,4 +51,18 @@ export default function jobs() {
             </div>
         </main>
     )
+}
+
+export const getStaticProps = async () => {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/jobs`, {
+        method: 'GET',
+        headers: {
+            'content-type': 'application/json',
+            // 'Authorization': token
+        }
+    });
+    const alljobs = await res.json();
+    return {
+        props: alljobs
+    }
 }
