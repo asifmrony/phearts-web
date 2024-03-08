21fc2ce3bf89d3ca1cb5cb466d80fcda5b9f0b12
Asif Mahmud Rony <asif@rockstreamer.com>
2024-03-08 16:31:48 +0600

    real sponsors added as data

--- components/home/Sponsors.jsx
+++ components/home/Sponsors.jsx
@@ -1,21 +1,21 @@
 import Image from "next/image"
-import sponsorOne from '@/data/sponsor-1.png'
-import sponsorTwo from '@/data/sponsor-2.png'
-import sponsorThree from '@/data/sponsor-3.png'
-import sponsorFour from '@/data/sponsor-4.png'
-import sponsorFive from '@/data/sponsor-5.png'
+import sponsorOne from '@/data/collaborations/columbia-university-logo-noback.png'
+import sponsorTwo from '@/data/collaborations/university_of_chicago_logo.jpg'
+import sponsorThree from '@/data/collaborations/Bangabandhu_Sheikh_Mujib_Medical_University_logo.png'
+import sponsorFour from '@/data/collaborations/icddrb_logo_transparent.png'
+import sponsorFive from '@/data/collaborations/Sam_Houston_State_University_logo.png'
 
 export default function Sponsors() {
     return (
         <div className='py-20'>
             <div className="hero-width w-full mx-auto text-center">
                 <span className='inline-block text-xs bg-themeblue text-white uppercase mb-5 p-2'>OUR COLLABORATIVE PARTNERS</span>
-                <div className="flex flex-wrap space-y-4 space-x-4 mt-10">
-                  <Image src={sponsorOne} alt="Sponsor Image" />
-                  <Image src={sponsorTwo} alt="Sponsor Image" /> 
-                  <Image src={sponsorThree} alt="Sponsor Image" />
-                  <Image src={sponsorFour} alt="Sponsor Image" />
-                  <Image src={sponsorFive} alt="Sponsor Image" />
+                <div className="grid5-5 mt-10">
+                  <Image src={sponsorOne} alt="Sponsor Image"  />
+                  <Image src={sponsorTwo} alt="Sponsor Image"  /> 
+                  <Image src={sponsorThree} alt="Sponsor Image"  />
+                  <Image src={sponsorFour} alt="Sponsor Image"  />
+                  <Image src={sponsorFive} alt="Sponsor Image"  />
                 </div>
             </div>
         </div>
