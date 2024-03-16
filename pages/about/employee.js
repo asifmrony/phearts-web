ef169b0150ae553a68ca4b8a47863e9f42c95ef8
Asif Mahmud Rony <asif@rockstreamer.com>
2024-03-16 17:13:02 +0600

    employee page text and responsive fix

--- pages/about/employee.js
+++ pages/about/employee.js
@@ -10,18 +10,18 @@ export default function employee(employeeList) {
         <table className={`w-2/3 mx-auto`}>
           <thead className='text-left bg-themegreen text-white'>
             <tr>
-              <th className='w-[30%] py-4 pl-9 text-lg font-bold'>Employee Name</th>
-              <th className='w-[40%] py-4 pl-9 text-lg font-bold'>Designation</th>
-              <th className='w-[30%] py-4 pl-9 text-lg font-bold'>Department</th>
+              <th className='w-[30%] py-4 pl-4 md:pl-9 text-sm md:text-lg font-bold'>Employee Name</th>
+              <th className='w-[40%] py-4 pl-4 md:pl-9 text-sm md:text-lg font-bold'>Designation</th>
+              <th className='w-[30%] py-4 pl-4 md:pl-9 text-sm md:text-lg font-bold'>Department</th>
             </tr>
           </thead>
-          <tbody className={`text-left ${poppins.variable} font-poppins`}>
+          <tbody className={`text-left ${poppins.variable} font-poppins text-black`}>
             {
               employeeList?.data.map((item) => (
                 <tr key={item?.id}>
-                  <td className='py-2 pl-9'>{item?.attributes.name}</td>
-                  <td className='py-2 pl-9'>{item?.attributes.designation}</td>
-                  <td className='py-2 pl-9'>{item?.attributes.department}</td>
+                  <td className='py-2 pl-4 md:pl-9'>{item?.attributes.name}</td>
+                  <td className='py-2 pl-4 md:pl-9'>{item?.attributes.designation}</td>
+                  <td className='py-2 pl-4 md:pl-9'>{item?.attributes.department}</td>
                 </tr>
               ))
             }
