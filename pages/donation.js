d25b1a465ddde5fe88095fce9669528cf0620af0
Asif Mahmud Rony <asif@rockstreamer.com>
2024-04-02 02:21:17 +0600

    final presentation based changes

--- pages/donation.js
+++ pages/donation.js
@@ -4,9 +4,15 @@ import React from 'react'
 export default function donation() {
   return (
     <div className='min-h-[100vh] bg-white flex justify-center items-center'>
-        <h1 className={`text-center ${poppins.variable} font-poppins text-black`}>
-          There are a number of ways <br></br> you may land a helping hand to improve one’s healthy life.
-        </h1>
+        <h1 className={`text-center ${poppins.variable} font-poppins text-black leading-loose`}>
+        <b>Account Name:</b> Population Health Research and Training Society <br></br> 
+        <b>Account Number:</b> 4303203791257001 <br></br>
+        <b>Bank Name:</b> BRAC Bank Ltd. <br></br>
+        <b>Branch Name:</b> Araihazar, Narayanganj <br></br>
+        <b>Contact Person:</b> A. K. M. Rabiul Hasan (Treasurer) <br></br>
+        <b>Contact Number:</b> 01760378256 <br></br>
+        <b>bkash Number:</b> 01705360348
+        </h1>        
     </div>
   )
 }
